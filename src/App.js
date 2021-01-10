ce69e3b052e3caab7e1bd403cbcebf0b06b7d713
dustin <dustnclaytiner@gmail.com>
2021-01-10 17:59:01 -0600

    header only

--- src/App.js
+++ src/App.js
@@ -1,21 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+// import React from 'react';
+// import './styles/App.css';
+// // import {BrowserRouter as Router, Route} from 'react-router-dom'
+import Header from './components/Header'
+// import Footer from './components/Footer';
+// import Examples from './components/Examples'
+// import About from './components/Capturing'
+// import Contact from './components/Contact'
+// import AboutMe from './components/AboutMe'
+
 
 function App() {
   return (
     <>
       <Header/>
-        <div class="page">
-          {/* <Route path='/About' component={About}/> */}
+        {/* <div class="page">
+          <Route path='/About' component={About}/>
           <About/>
-          {/* <Route path='/Merchandise' component={Moonraker}/> */}
+          <Route path='/Merchandise' component={Moonraker}/>
           <AboutMe/>
           <Route path='/Contact' component={Contact}/>
-          {/* <Contact/>           */}
-          {/* <Route exact path='/' component={Examples}/> */}
+          <Contact/>          
+          <Route exact path='/' component={Examples}/>
           <Examples/>
         </div>
-      <Footer/> 
+      <Footer/>  */}
     </>
   );
 }
