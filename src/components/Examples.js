93963dc0813323139f69b68680e45dbf6bb3fc96
dustin <dustnclaytiner@gmail.com>
2021-01-10 21:30:19 -0600

    fixing component

--- src/components/Examples.js
+++ src/components/Examples.js
@@ -1,20 +1,6 @@
 import React from 'react';
 import "../styles/Examples.css"
 
-const Work = () => {
-    return(
-        <>
-            <h1 class="title">Examples of our work</h1>
-            <div>
-                
-                <img class="image" src='MMC_Icon_Gold.jpg' alt='earrings'/>
-                <img class="image" src='MMC_Icon_Gold.jpg' alt='earrings'/>
-                <img class="image" src='MMC_Icon_Gold.jpg' alt='earrings'/>
-            </div>
-        </>
-    )
-}
-
 const Wor = ()=>{
     return(
         <>
@@ -27,7 +13,6 @@ const Wor = ()=>{
             </div>
         </>
     )
-
 }
 
 export default Wor
