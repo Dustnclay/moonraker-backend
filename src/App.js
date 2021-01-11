d22a8f8575088d866ebc7cb7645e537f75cc4e55
dustin <dustnclaytiner@gmail.com>
2021-01-10 20:43:37 -0600

    about added

--- src/App.js
+++ src/App.js
@@ -13,16 +13,16 @@ function App() {
   return (
     <>
       <Header/>
-        {/* <div class="page">
-          <Route path='/About' component={About}/>
+        <div class="page">
+          {/* <Route path='/About' component={About}/> */}
           <About/>
-          <Route path='/Merchandise' component={Moonraker}/>
+          {/* <Route path='/Merchandise' component={Moonraker}/>
           <AboutMe/>
           <Route path='/Contact' component={Contact}/>
           <Contact/>          
           <Route exact path='/' component={Examples}/>
-          <Examples/>
-        </div> */}
+          <Examples/> */}
+        </div>
       <Footer/> 
     </>
   );
