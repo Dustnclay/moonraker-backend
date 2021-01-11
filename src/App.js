881410b8e6dbd80544e4ef6c6c4761b4ce94954e
dustin <dustnclaytiner@gmail.com>
2021-01-10 21:02:33 -0600

    Examples added

--- src/App.js
+++ src/App.js
@@ -3,9 +3,9 @@
 // // import {BrowserRouter as Router, Route} from 'react-router-dom'
 import Header from './components/Header'
 import Footer from './components/Footer';
-// import Examples from './components/Examples'
+import Examples from './components/Examples'
 import Capturing from './components/Capturing'
-// import Contact from './components/Contact'
+import Contact from './components/Contact'
 import AboutMe from './components/AboutMe'
 
 
@@ -18,10 +18,10 @@ function App() {
           <Capturing/>
           {/* <Route path='/Merchandise' component={Moonraker}/> */}
           <AboutMe/>
-          {/* <Route path='/Contact' component={Contact}/>
+          {/* <Route path='/Contact' component={Contact}/> */}
           <Contact/>          
-          <Route exact path='/' component={Examples}/>
-          <Examples/> */}
+          {/* <Route exact path='/' component={Examples}/> */}
+          <Examples/>
         </div>
       <Footer/> 
     </>
