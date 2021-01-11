97de5b6721c1d2a285d168974de3e018e723c9a4
dustin <dustnclaytiner@gmail.com>
2021-01-10 21:45:35 -0600

    working on comp

--- src/components/Examples.js
+++ src/components/Examples.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import "../styles/Examples.css"
 
-const Wor = ()=>{
+const Works = ()=>{
     return(
         <>
             <h1 class="title">Examples of our work</h1>
             <div>
-                
+                <h6>temp</h6>
                 <img class="image" src='MMC_Icon_Gold.jpg' alt='earrings'/>
                 <img class="image" src='MMC_Icon_Gold.jpg' alt='earrings'/>
                 <img class="image" src='MMC_Icon_Gold.jpg' alt='earrings'/>
@@ -15,5 +15,5 @@ const Wor = ()=>{
     )
 }
 
-export default Wor
+export default Works
 
