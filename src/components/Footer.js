75b069253863a43fd3c5e49856a569189d1236e6
Dustin <DustinTiner@Emilys-MacBook-Air.local>
2021-03-04 19:31:26 -0600

    vimeo link added

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -9,7 +9,10 @@ const Footer = () => {
                 <img class="footerImage" src={"/moonraker-text-under.jpeg"} alt="logo"/>
                 <div class="footerlinksdiv">
                     <a class="footerlink" href="https://www.facebook.com/Moonraker-Media-Co-103785678200787" target="_blank"rel="noreferrer">FACEBOOK</a> 
-                    <a class="footerlink" href="https://www.youtube.com/channel/UCS0ETeAkqiuEMFsZ5hrxXMQ" target="_blank" rel="noreferrer">YOUTUBE</a>                 
+                    <a class="footerlink" href="https://www.youtube.com/channel/UCS0ETeAkqiuEMFsZ5hrxXMQ" target="_blank" rel="noreferrer">YOUTUBE</a>  
+                    <a class="footerlink" href="https://vimeo.com/user128951689 " target="_blank" rel="noreferrer">VIMEO</a>
+
+                                  
                 </div>                
             </div>
             <span class="footerdiv">
